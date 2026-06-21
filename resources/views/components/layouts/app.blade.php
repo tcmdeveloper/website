b86210e2d8359098dcd7beab4d6928c8b06c1290
Frank Jones <frankjones@Franks-MacBook-Pro.local>
2026-06-21 09:50:10 +0100

    favicon added

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -7,7 +7,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
+    {{-- Page title --}}
     <title>{{ $title ?? config('app.name') }}</title>
+
+    {{-- Favicons --}}
+    <link rel="icon" href="{{ asset('favicon.ico') }}" sizes="any">
+    <link rel="icon" type="image/png" href="{{ asset('favicon-32x32.png') }}">
+    <link rel="apple-touch-icon" href="{{ asset('apple-touch-icon.png') }}">
 
     {{-- Vite --}}
     @vite(['resources/css/app.css', 'resources/js/app.js'])
@@ -23,23 +29,20 @@
     class="pt-[61px] flex flex-col min-h-screen bg-gray-50 text-gray-800 font-body"
 >
 
-    {{-- NAVBAR --}}
     <x-layouts.navigation />
 
-    {{-- Page headings --}}
     <x-ui.page-headings
         :title="$title ?? null"
         :subtitle="$subtitle ?? null"
     />
 
-    {{-- MAIN CONTENT --}}
     <main class="flex-1">
         {{ $slot }}
     </main>
 
-    {{-- FOOTER --}}
     <x-layouts.footer />
 
     @stack('scripts')
+
 </body>
 </html>
