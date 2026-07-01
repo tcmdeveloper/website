3bc967f1aeee639fb837fab0d5584e84e63cded6
Frank Jones <frankjones@Franks-MacBook-Pro.local>
2026-07-02 00:19:48 +0100

    about page in container

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -3,18 +3,19 @@
 
 <x-layouts.app 
     title="About Metrix" 
-    subtitle="What we do at True Crime Metrix."
+    subtitle="Who is this guy?"
 >
 
+    <x-ui.container class="max-w-5xl">
+        <x-ui.card class="max-w-2xl mx-auto text-center prose-content">
 
-    <x-ui.card class="max-w-2xl mx-auto text-center prose-content">
+            <p class="mb-3">I am Metrix, the creator behind True Crime Metrix.</p>
+            <p class="mb-3">I mostly live stream extensive running commentary on witness testimonies, interrogations, victim impact statements and attorney's closing arguments.</p>
+            <p class="mb-3">Sharing thoughts and insights on some of the most interesting criminal cases of recent times. We include much 'gallows humour' in our conversations.</p>
+            <p>Hopefully that will keep us all sane as we inspect some of the most awful situations and characters known to the waking life of human behaviour.</p>
 
-        <p class="mb-3">I am Metrix, the creator behind True Crime Metrix.</p>
-        <p class="mb-3">I mostly live stream extensive running commentary on witness testimonies, interrogations, victim impact statements and attorney's closing arguments.</p>
-        <p class="mb-3">Sharing thoughts and insights on some of the most interesting criminal cases of recent times. We include much 'gallows humour' in our conversations.</p>
-        <p>Hopefully that will keep us all sane as we inspect some of the most awful situations and characters known to the waking life of human behaviour.</p>
-
-    </x-ui.card>
+        </x-ui.card>
+    </x-ui.container>
 
 
 </x-layouts.app>
