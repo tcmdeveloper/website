ac3f3ae232dfe34ab1d07c12007d9a47ce207573
Frank Jones <frankjones@Franks-MacBook-Pro.local>
2026-07-03 20:49:58 +0100

    navigation whitespace-nowrap - deployment

--- resources/views/components/layouts/navigation.blade.php
+++ resources/views/components/layouts/navigation.blade.php
@@ -104,11 +104,11 @@
         </div>
 
         {{-- Menu items --}}
-        <div class="w-full p-8 mt-32 flex flex-col items-center space-y-5 text-6xl font-heading font-bold">
-            <a href="{{ route('cases.index') }}" class="flex items-center justify-center  text-zinc-100 hover:text-yellow-300">Criminal cases</a>
-            <a href="{{ route('articles.index') }}" class="flex items-center justify-center  text-zinc-100 hover:text-yellow-300">Articles</a>
-            <a href="{{ route('categories.index') }}" class="flex items-center justify-center  text-zinc-100 hover:text-yellow-300">Categories</a>
-            <a href="{{ route('contact.show') }}" class="flex items-center justify-center  text-zinc-100 hover:text-yellow-300">Contact me</a>
+        <div class="w-full p-8 mt-32 flex flex-col items-center space-y-5 text-5xl md:text-6xl font-heading font-bold">
+            <a href="{{ route('cases.index') }}" class="whitespace-nowrap text-zinc-100 hover:text-yellow-300">Cases</a>
+            <a href="{{ route('articles.index') }}" class="whitespace-nowrap text-zinc-100 hover:text-yellow-300">Articles</a>
+            <a href="{{ route('categories.index') }}" class="whitespace-nowrap text-zinc-100 hover:text-yellow-300">Categories</a>
+            <a href="{{ route('contact.show') }}" class="whitespace-nowrap text-zinc-100 hover:text-yellow-300">Contact me</a>
         </div>
 
     </div>
