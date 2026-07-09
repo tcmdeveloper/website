b4a065a44770a6755aeb8c0f976989dad928943f
Frank Jones <frankjones@Franks-MacBook-Pro.local>
2026-07-09 21:48:36 +0100

    menu-items labels added

--- resources/views/components/layouts/navigation.blade.php
+++ resources/views/components/layouts/navigation.blade.php
@@ -4,7 +4,7 @@
     x-data="{ menuOpen: false, searchOpen: false }"
     @click.away="searchOpen = false"
     class="
-        fixed top-0 left-0 w-full h-[61px] z-50 
+        fixed top-0 left-0 w-full h-[61px] z-50
         flex
         px-6 py-4
         bg-zinc-900 text-sm font-light
@@ -17,20 +17,19 @@
     {{-- Left side --}}
     <div class="justify-self-start lg:w-80 md:w-64 w-16">
         <a href="/">
-            <img 
+            <img
                 src="{{ asset('images/true-crime-metrix-logo-xs.png') }}"
                 srcset="
-                    /images/true-crime-metrix-logo-xs.png 64w,
-                    /images/true-crime-metrix-logo-sm.png 128w,
-                    /images/true-crime-metrix-logo.png 256w
+                    {{ asset('images/true-crime-metrix-logo-xs.png') }} 64w,
+                    {{ asset('images/true-crime-metrix-logo-sm.png') }} 128w,
+                    {{ asset('images/true-crime-metrix-logo.png') }} 256w
                 "
                 sizes="auto"
                 alt="True Crime Metrix"
-                class="h-7 aspect-square"
-            >            
+                class="h-7 w-7"
+            >
         </a>
     </div>
-
 
     {{-- Center --}}
     <div class="justify-self-center flex-1 text-center">
@@ -38,7 +37,7 @@
             <span
                 class="
                     inline-block
-                    transition-transform duration-300 
+                    transition-transform duration-300
                     group-hover:translate-y-[2px] group-hover:scale-[101%]
                 "
             >
@@ -47,17 +46,30 @@
         </a>
     </div>
 
-
     {{-- Right side --}}
     <div class="justify-self-end flex justify-end items-center gap-5 lg:w-80 md:w-64 w-16">
 
         {{-- Main menu button --}}
-        <button @click="menuOpen = true; searchOpen = false" class="nav-link">
+        <button
+            @click="menuOpen = true; searchOpen = false"
+            class="nav-link"
+            type="button"
+            aria-label="Open navigation menu"
+            :aria-expanded="menuOpen"
+            aria-controls="main-menu"
+        >
             <x-ui.icon name="bars-3" />
         </button>
 
         {{-- Search button --}}
-        <button @click="searchOpen = !searchOpen; menuOpen = false" class="nav-link md:inline-flex hidden">
+        <button
+            @click="searchOpen = !searchOpen; menuOpen = false"
+            class="nav-link md:inline-flex hidden"
+            type="button"
+            aria-label="Toggle search"
+            :aria-expanded="searchOpen"
+            aria-controls="search-dropdown"
+        >
             <x-ui.icon name="magnifying-glass" />
         </button>
 
@@ -68,6 +80,7 @@
 
             <form method="POST" action="{{ route('logout') }}" class="md:inline-flex hidden">
                 @csrf
+
                 <x-ui.text-button type="submit" class="nav-link">
                     Log out
                 </x-ui.text-button>
@@ -80,7 +93,6 @@
 
     </div>
 
-
     {{-- BACKDROP --}}
     <div
         x-cloak
@@ -88,11 +100,12 @@
         x-transition.opacity
         class="fixed inset-0 bg-zinc-900/95 z-40"
         @click="menuOpen = false"
+        aria-hidden="true"
     ></div>
-
 
     {{-- FULL SCREEN DRIFT MENU --}}
     <div
+        id="main-menu"
         x-cloak
         class="fixed inset-0 z-50"
         x-show="menuOpen"
@@ -102,27 +115,46 @@
         x-transition:leave="transition ease-in duration-200"
         x-transition:leave-start="translate-x-0"
         x-transition:leave-end="-translate-x-full"
+        role="dialog"
+        aria-modal="true"
+        aria-label="Navigation menu"
     >
+
         {{-- Top row --}}
         <div class="h-[61px] pr-10 flex justify-end items-center border-b border-zinc-600">
-            <x-ui.text-button @click="menuOpen = false" class="text-zinc-300 hover:text-white text-4xl!">
+            <x-ui.text-button
+                @click="menuOpen = false"
+                class="text-zinc-300 hover:text-white text-4xl!"
+                aria-label="Close navigation menu"
+            >
                 ✕
             </x-ui.text-button>
         </div>
 
         {{-- Menu items --}}
         <div class="w-full p-8 mt-32 flex flex-col items-center space-y-5 text-5xl md:text-6xl font-heading font-bold">
-            <a href="{{ route('cases.index') }}" class="whitespace-nowrap text-zinc-100 hover:text-yellow-300">Cases</a>
-            <a href="{{ route('articles.index') }}" class="whitespace-nowrap text-zinc-100 hover:text-yellow-300">Articles</a>
-            <a href="{{ route('categories.index') }}" class="whitespace-nowrap text-zinc-100 hover:text-yellow-300">Categories</a>
-            <a href="{{ route('contact.show') }}" class="whitespace-nowrap text-zinc-100 hover:text-yellow-300">Contact me</a>
+            <a href="{{ route('cases.index') }}" class="whitespace-nowrap text-zinc-100 hover:text-yellow-300">
+                Cases
+            </a>
+
+            <a href="{{ route('articles.index') }}" class="whitespace-nowrap text-zinc-100 hover:text-yellow-300">
+                Articles
+            </a>
+
+            <a href="{{ route('categories.index') }}" class="whitespace-nowrap text-zinc-100 hover:text-yellow-300">
+                Categories
+            </a>
+
+            <a href="{{ route('contact.show') }}" class="whitespace-nowrap text-zinc-100 hover:text-yellow-300">
+                Contact me
+            </a>
         </div>
 
     </div>
 
-
     {{-- SEARCH DROPDOWN --}}
     <div
+        id="search-dropdown"
         x-cloak
         x-show="searchOpen"
         x-transition:enter="transition ease-out duration-200"
@@ -134,7 +166,7 @@
         class="absolute top-full left-0 w-full bg-stone-300 border-b border-b-stone-400 shadow-xl px-6 py-4 z-40"
     >
         <form action="/search" method="GET" class="max-w-3xl mx-auto">
-            {{-- Search input --}}
+
             <div>
                 <x-ui.input
                     name="q"
@@ -146,8 +178,8 @@
                     autofocus
                 />
             </div>
+
         </form>
     </div>
-
 
 </nav>
